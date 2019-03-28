90c58a82c3396a23faba23ce5965057c227b71c6
anastasiakova <kovalkov@post.bgu.ac.il>
2019-03-28 17:31:12 +0200

    user class

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,5 +1,30 @@
 package Model;
+import java.util.List;
 
-public class User {
+public class User implements ISQLable {
+    String id;
+    String Email;
+    List<String> favoriteCategories;
+    List<CalendarSession> imTheTeacher;
+    List<CalendarSession> imTheStudent;
 
+    @Override
+    public String getPrimaryKey() {
+        return null;
+    }
+
+    @Override
+    public String getPrimaryKeyName() {
+        return null;
+    }
+
+    @Override
+    public String getTableFields() {
+        return null;
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
 }
